c4a42787282496d556d11fa3a0098b233c687444
booo <borgers@mi.fu-berlin.de>
2011-09-09 16:50:42 +0200

    run-test.js: beautified

--- run-test.js
+++ run-test.js
@@ -16,13 +16,13 @@ jsdom.env('<html><body></body></html>', function(errors, window) {
         }
         eval(i+"=window['"+i+"'];");
     }
-    OpenLayers = require("./lib/OpenLayers.js")['OpenLayers'];
+    OpenLayers = require("./lib/OpenLayers.js").OpenLayers;
     var map = new OpenLayers.Map(document.createElement("map"));
     map.addLayer(new OpenLayers.Layer("", {isBaseLayer:true}));
     map.setCenter(new OpenLayers.LonLat(-71,42), 10);
     var px = map.getPixelFromLonLat(map.getLonLatFromPixel(new OpenLayers.Pixel(100,100)));
     console.log(px);
-    var px = map.getLonLatFromPixel(map.getPixelFromLonLat(new OpenLayers.LonLat(10,10)));
+    px = map.getLonLatFromPixel(map.getPixelFromLonLat(new OpenLayers.LonLat(10,10)));
     console.log(px);
 
 });
